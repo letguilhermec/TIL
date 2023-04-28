73f4251464adfa09937535d198beb0ca9701fbdc
Guilherme Correa <correacgui@gmail.com>
2023-04-28 14:28:52 -0300

    Adds TextField to AddThingsView()

--- TIL/AddThingView.swift
+++ TIL/AddThingView.swift
@@ -3,9 +3,13 @@ import SwiftUI
 struct AddThingView: View {
   @Environment(\.dismiss) var dismiss
   @Binding var someThings: ThingStore
+  @State private var thing = ""
 
   var body: some View {
     VStack {
+      TextField("Thing I Learned", text: $thing)
+        .textFieldStyle(.roundedBorder)
+        .padding()
       Button("Done") {
         someThings.things.append("FOMO")
         dismiss()
